2350ddaea6b85e905b8602615d0d32b1a5824bbf
Izabella <bellacbrandao@gmail.com>
2021-06-04 16:17:23 -0300

    renderizando listas - insta4

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -10,32 +10,42 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = {
+    postsUsuarios: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'Izabella',
+        fotoUsuario: 'https://picsum.photos/51/51',
+        fotoPost: 'https://picsum.photos/201/151'
+      },
+      {
+        nomeUsuario: 'Wellington',
+        fotoUsuario: 'https://picsum.photos/52/52',
+        fotoPost: 'https://picsum.photos/202/152'
+      }
+    ]
+  }
+
   render() {
+
+    const postDeUsuarios = this.state.postsUsuarios.map((usuario) => {
+      return (
+        <Post>
+          {usuario.nomeUsuario}
+          {usuario.fotoUsuario}
+          {usuario.fotoPost}
+        </Post>
+      );
+    });
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'Izabella'}
-          fotoUsuario={'https://picsum.photos/51/51'}
-          fotoPost={'https://picsum.photos/201/151'}
-        />
-
-        <Post
-          nomeUsuario={'Eusa'}
-          fotoUsuario={'https://picsum.photos/52/52'}
-          fotoPost={'https://picsum.photos/202/152'}
-        />
-
-        <Post
-          nomeUsuario={'Daniella'}
-          fotoUsuario={'https://picsum.photos/53/53'}
-          fotoPost={'https://picsum.photos/203/153'}
-        />
+        {postDeUsuarios}
       </MainContainer>
     );
   }
