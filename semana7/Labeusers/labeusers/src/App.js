3ef4273bb0a017791686bb609f0be3949d0e771f
Izabella <bellacbrandao@gmail.com>
2021-06-15 23:21:04 -0300

    adicionando funcao de apagar

--- semana7/Labeusers/labeusers/src/App.js
+++ semana7/Labeusers/labeusers/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Cadastro} from './components/Cadastro'
+import {MostrarDetalheUsuario} from './components/MostrarDetalheUsuario'
 import axios from "axios"
 
 const url =
@@ -19,6 +20,10 @@ export default class App extends React.Component{
     mostrarLista: false
   }
 
+  componentDidMount() {
+    this.getRegisters();
+  }
+
   onChangeName = (event) => {
     this.setState({inputName: event.target.value})
   }
@@ -36,13 +41,14 @@ export default class App extends React.Component{
     axios
     .post(url,body, headers)
     .then((res) => {
-      // alert ("Cadastro efetuado com sucesso")
-      console.log(res)
+      alert ("Cadastro efetuado com sucesso")
+      console.log("res post", res)
       this.setState({inputName: "", inputEmail: ""})
+      this.getRegisters()
     })
     .catch((err) => {
-      console.log(err)
-      // alert(err)
+      console.log("err post",err)
+      alert("Erro ao cadastrar usuario")
     })
   }
 
@@ -58,21 +64,45 @@ export default class App extends React.Component{
       .get(url, headers)
       .then((res) => {
         console.log("res get", res)
-        // this.setState({ users: res.data.result.list });
+        this.setState({ users: res.data });
       })
       .catch((err) => {
-        console.log("err get", err.response.data)
+        console.log("err get", err)
         // alert(err.response.data.message);
       });
 
   };
 
+  onClickApagarUsuario = (id) => {
+    let confirmacao = window.confirm("Tem certeza que deseja apagar?")
+    if(confirmacao){
+      axios
+      .delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`, headers)
+      .then((res) => {
+        console.log("res get", res)
+        alert("usuario excluido com sucesso")
+        this.getRegisters()
+      })
+      .catch((err) => {
+        console.log("err get", err)
+        // alert(err.response.data.message);
+      });
+    }
+  }
+
+  mostrarDetalhe = (id) => {
+    <MostrarDetalheUsuario/>
+  }
+
   render(){
+
+    console.log(this.state.users)
 
     const mostrarLista = this.state.users.map((user) => {
       return<li key={user.id}>
         {user.name}
-        <button>Apagar</button>
+        <button onClick={() => this.onClickApagarUsuario(user.id)}>Apagar</button>
+        <button onClick={() => this.mostrarDetalhe(user.id)}>Mostrar detalhes do usuario</button>
       </li>
     })
     return(
@@ -85,7 +115,8 @@ export default class App extends React.Component{
         createRegister={this.createRegister}
         />
         <button onClick={this.getRegisters}>Ver Lista</button>
-        {this.state.mostrarLista} ? {mostrarLista} : <></>
+
+        {this.state.mostrarLista === true ? mostrarLista : <></>}
 
       </div>
     );
---
+++ semana7/Labeusers/labeusers/src/components/MostrarDetalheUsuario.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class MostrarDetalheUsuario extends React.Component{
+    render(){
+        return(
+            <div>
+                Detalhes do Usuario
+            </div>
+        );
+    }
+}
