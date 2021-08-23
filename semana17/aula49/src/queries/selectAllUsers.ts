0578f98a58d4e3234153c75086b5b0c51396e025
Izabella <bellacbrandao@gmail.com>
2021-08-23 16:07:14 -0300

    exercicio completo

--- semana17/aula49/src/queries/selectAllUsers.ts
+++ semana17/aula49/src/queries/selectAllUsers.ts
@@ -1,7 +1,10 @@
 import { connection } from "../data/connection"
 
-export default async function selectAllUsers(sort: string, order:string, size: number, offset: number):Promise<any> {
+export default async function selectAllUsers(name: string, type: string,sort: string, order:string, size: number, offset: number):Promise<any> {
+    
     const result = await connection("aula48_exercicio")
+    .where("name", "LIKE", `%${name}%`)
+    .where("type", "LIKE", `%${type}%`)
     .orderBy(sort,order)
     .limit(size)
     .offset(offset)
