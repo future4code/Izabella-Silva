058344de3e655c50c5b88170339049f1cba28ff0
izabellacbrandao <bellacbrandao@gmail.com>
2021-09-07 18:30:22 -0300

    aula 53

---
+++ Semana19/aula55/src/index.ts
@@ -0,0 +1,12 @@
+// import app from "./app"
+// import editUser from './endpoints/editUser'
+// import createUser from './endpoints/createUser'
+import {v4} from 'uuid'
+
+const id = v4()
+
+console.log("Generated Id", id)
+
+// app.post('/users/signup', createUser)
+// app.put('/users/:id', editUser)
+
